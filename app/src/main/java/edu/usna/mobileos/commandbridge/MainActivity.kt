f7db41a954b177c3816bfd70474f2047bd7a9f73
Everett Stenberg <m226252@usna.edu>
2022-04-12 13:55:51 -0400

    push1 attempt

---
+++ app/src/main/java/edu/usna/mobileos/commandbridge/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.usna.mobileos.commandbridge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
