f48786a16eb0dacc41e7a370f88ec66853aaadf8
Steinshark <everettperson@gmail.com>
2022-04-14 15:13:34 -0400

    4-14 update, BT can connect

--- app/src/main/java/edu/usna/mobileos/commandbridge/MainActivity.kt
+++ app/src/main/java/edu/usna/mobileos/commandbridge/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.usna.mobileos.commandbridge
 
+import android.Manifest
 import android.app.Service
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
@@ -9,12 +10,15 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import org.json.JSONObject
 import java.net.HttpURLConnection
 import java.net.Socket
@@ -33,128 +37,28 @@ class MainActivity : AppCompatActivity() {
     val uuid        = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
     lateinit var socket: BluetoothSocket
 
-
-
-
-    lateinit var displayText: TextView
-    lateinit var jsonOBJ: JSONObject
-    val intentReceiver = object: BroadcastReceiver(){
-        override fun onReceive(p0: Context?, p1: Intent?) {
-
-            //Grab the message from the thing
-            val feedString = p1?.getStringExtra("message")
-            displayText.text = feedString
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btManager = getSystemService(BluetoothManager::class.java) as (BluetoothManager)
         btAdapter = btManager.getAdapter()
-
-
-
-        val apiKey = "646463100831aeea0ccec00b18447d24"
-        val location = "38.9784,-76.4922"
-        val urlString = "https://api.forecast.io/forecast/$apiKey/$location"
-
-
-
-
-
-
-
-
-        displayText = findViewById(R.id.displayText)
-
-        val intentFilter = IntentFilter()
-        intentFilter.addAction("JSON_RETRIEVED") //note the same action as broadcast by the Service
-        registerReceiver(intentReceiver, intentFilter)
-
-        startService(
-            Intent(baseContext, ReadJSONService::class.java).putExtra(
-                "urlString",
-                urlString
-            )
-        )
     }
 
 
-    fun connectBluetooth(){
+    fun connectBluetooth(v: View?){
         if(btAdapter == null){
             Toast.makeText(this,"Bluetooth unavailable",Toast.LENGTH_LONG).show()
         }
-        else{
-            btDevice    = btAdapter.getRemoteDevice("8C:DE::00:01:8A:2C")
-            socket      = btDevice.createRfcommSocketToServiceRecord(uuid)
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 2);
+        }
+        btDevice    = btAdapter.getRemoteDevice("8C:DE:00:01:8A:2C")
+        socket      = btDevice.createRfcommSocketToServiceRecord(uuid)
+
+        if(socket == null){
+            Toast.makeText(this,"Socket Null",Toast.LENGTH_LONG).show()
 
         }
-    }
-
-
-    private fun retrieveSiteContent(urlString: String): String {
-
-        var returnString = ""
-        try {
-            val url = URL(urlString)
-            val urlConnection = url.openConnection() as HttpURLConnection
-            val reader = urlConnection.inputStream.bufferedReader()
-            returnString = reader.readText()
-            reader.close()
-            urlConnection.disconnect()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return returnString
-    }
-}
-
-class ReadJSONService : Service() {
-
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        doWork(intent)
-        return START_STICKY
-    }
-
-    private fun doWork(intent: Intent?) {
-        val urlString = intent?.getStringExtra("urlString") ?: ""
-        val myThread = Thread {
-            Log.i("IT472", "retrieving JSON from $urlString")
-            val feedString = retrieveSiteContent(urlString)
-            Log.i("IT472", "received $feedString")
-
-            //broadcast intent
-            val broadcastIntent = Intent()
-            broadcastIntent.putExtra("message", feedString)
-            broadcastIntent.action = "JSON_RETRIEVED"
-            baseContext.sendBroadcast(broadcastIntent)
-
-            //stop the service
-            stopSelf()
-        }
-        myThread.start()
-    }
-
-
-    private fun retrieveSiteContent(urlString: String): String {
-
-        var returnString = ""
-        try {
-            val url = URL(urlString)
-            val urlConnection = url.openConnection() as HttpURLConnection
-            val reader = urlConnection.inputStream.bufferedReader()
-            returnString = reader.readText()
-            reader.close()
-            urlConnection.disconnect()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return returnString
     }
 }
