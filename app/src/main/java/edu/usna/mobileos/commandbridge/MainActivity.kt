5a6ad94f49f93feb68846425ee9a4009a277c7df
Everett Stenberg <m226252@usna.edu>
2022-04-18 14:23:11 -0400

    Mile1 update

--- app/src/main/java/edu/usna/mobileos/commandbridge/MainActivity.kt
+++ app/src/main/java/edu/usna/mobileos/commandbridge/MainActivity.kt
@@ -27,40 +27,62 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     // Class inits
+    var btCapable = true
 
-    //BLUETOOTH
+    //BLUETOOTH INITS 
     lateinit var btManager: BluetoothManager
     lateinit var btAdapter: BluetoothAdapter
     lateinit var btDevice: BluetoothDevice
-
-    // NETWORK
-    val uuid        = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+    
+    //BLUETOOTH  
+    val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
     lateinit var socket: BluetoothSocket
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Check and init the BT manager and adapter
         btManager = getSystemService(BluetoothManager::class.java) as (BluetoothManager)
-        btAdapter = btManager.getAdapter()
+        if(btManager.adapter == null){
+            Toast.makeText(baseContext,"No Bluetooth Adapter Found",Toast.LENGTH_LONG).show()
+            btCapable = false
+        }
+        else{
+            btAdapter = btManager.getAdapter()
+        }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        socket.close()
+    }
 
+    // Function to connect bluetooth device.
+    // Attatched to a button in the GUI.
     fun connectBluetooth(v: View?){
-        if(btAdapter == null){
+
+        //Check that device is btCapable and that adapter exists
+        if(btCapable == false || btAdapter == null){
             Toast.makeText(this,"Bluetooth unavailable",Toast.LENGTH_LONG).show()
+            return
         }
+
+        //Check that BT permissions were allowed by user, and if not, ask them for permission
         if (ActivityCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 2);
         }
+
+        //Attempt The bluetooth pairing and create a socket to the device
         btDevice    = btAdapter.getRemoteDevice("8C:DE:00:01:8A:2C")
         socket      = btDevice.createRfcommSocketToServiceRecord(uuid)
 
+        //if socket null, then we're out
         if(socket == null){
             Toast.makeText(this,"Socket Null",Toast.LENGTH_LONG).show()
         }
         else{
-            Toast.makeText(this,"Socket Connected",Toast.LENGTH_LONG).show()
+            Toast.makeText(this,"Socket initialized",Toast.LENGTH_LONG).show()
         }
     }
 }
