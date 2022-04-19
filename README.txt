59a0626c9a580c1ad9cc9cc7992023253add59d8
Steinshark <everettperson@gmail.com>
2022-04-18 21:56:30 -0400

    updated

---
+++ README.txt
@@ -0,0 +1,12 @@
+EVERETT STENBERG
+m226252
+TorqueBuddy 
+
+Feature:
+
+The most challenging part for my project will be handling and establishing bluetooth connections. The protocl for this 
+was not covered in class, so I am on my own here. I managed to get a working solution that runs the bluetooth BR/EDR 
+protocol (essentially all devices that are not bluetooth speakers). I have successfully implemented the API as per the 
+Android dveloper docs. My solution has code to do everything up to creating the socket that requests and responses 
+will pass through.
+
--- app/src/main/java/edu/usna/mobileos/commandbridge/MainActivity.kt
+++ app/src/main/java/edu/usna/mobileos/commandbridge/MainActivity.kt
@@ -1,43 +1,33 @@
 package edu.usna.mobileos.commandbridge
 
 import android.Manifest
-import android.app.Service
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
-import android.bluetooth.BluetoothSocket
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.bluetooth.*
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.IBinder
 import android.util.Log
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import org.json.JSONObject
-import java.net.HttpURLConnection
-import java.net.Socket
-import java.net.URL
+import java.io.IOException
+import java.lang.reflect.Method
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     // Class inits
     var btCapable = true
 
-    //BLUETOOTH INITS 
+    //Bluetooth environment
     lateinit var btManager: BluetoothManager
     lateinit var btAdapter: BluetoothAdapter
-    lateinit var btDevice: BluetoothDevice
-    
-    //BLUETOOTH  
-    val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
     lateinit var socket: BluetoothSocket
+    //Bluetooth variables
+    val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+    val macAddress  = "8C:DE:00:01:8A:2C"
 
+
+    // OVERRIDE THE ON CREATE FUNCTION
+    // SETUP THE BLUETOOTH ENVIRONMENT
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -46,43 +36,49 @@ class MainActivity : AppCompatActivity() {
         btManager = getSystemService(BluetoothManager::class.java) as (BluetoothManager)
         if(btManager.adapter == null){
             Toast.makeText(baseContext,"No Bluetooth Adapter Found",Toast.LENGTH_LONG).show()
-            btCapable = false
-        }
+            btCapable = false}
         else{
-            btAdapter = btManager.getAdapter()
-        }
+            btAdapter = btManager.getAdapter()}
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        socket.close()
+        // ensure the Bluetooth socket is closed
+        //socket.close()
     }
 
     // Function to connect bluetooth device.
-    // Attatched to a button in the GUI.
+    // Attached to a button in the GUI.
     fun connectBluetooth(v: View?){
+        val t = Thread {
+            //Check that device is btCapable and that adapter exists
+            if (btCapable == false || btAdapter == null) {
+                runOnUiThread{
+                    Toast.makeText(this, "Bluetooth unavailable", Toast.LENGTH_LONG).show()}
+                return@Thread}
 
-        //Check that device is btCapable and that adapter exists
-        if(btCapable == false || btAdapter == null){
-            Toast.makeText(this,"Bluetooth unavailable",Toast.LENGTH_LONG).show()
-            return
-        }
+            //Check that BT permissions were allowed by user, and if not, ask them for permission
+            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.BLUETOOTH_CONNECT),2)}
 
-        //Check that BT permissions were allowed by user, and if not, ask them for permission
-        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 2);
-        }
 
-        //Attempt The bluetooth pairing and create a socket to the device
-        btDevice    = btAdapter.getRemoteDevice("8C:DE:00:01:8A:2C")
-        socket      = btDevice.createRfcommSocketToServiceRecord(uuid)
+            //Get Device -> in this case it is an OBD 2 scanner
+            //Attempt to create a socket with the device
+            val btDevice = btAdapter.getRemoteDevice(macAddress)
+            socket = btDevice.createInsecureRfcommSocketToServiceRecord(uuid)
+            try {
+                socket.connect()
+            }
+            catch (e: IOException) {
+                runOnUiThread{
+                    Toast.makeText(this, "${e.message} - EXPECTED", Toast.LENGTH_SHORT).show()}
+                return@Thread}
 
-        //if socket null, then we're out
-        if(socket == null){
-            Toast.makeText(this,"Socket Null",Toast.LENGTH_LONG).show()
+            //Ensure that the socket is actually connected
+            if (socket.isConnected) {
+                runOnUiThread{
+                    Toast.makeText(this, "Socket initialized", Toast.LENGTH_LONG).show()}}
         }
-        else{
-            Toast.makeText(this,"Socket initialized",Toast.LENGTH_LONG).show()
-        }
+        t.start()
     }
 }
